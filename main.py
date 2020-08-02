a5756b497cb7cf9eed2e4d5d023428a5203ece30
Damie <jogungbure@gmail.com>
2020-08-02 22:45:18 +0100

    Init commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tessaract OCR\tesseract.exe"
+img = Image.open("img.jpg")
+text = pytesseract.image_to_string(img)
+print(text)
