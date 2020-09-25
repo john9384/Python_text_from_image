b36cbdfbd078531cfc5db5df53af10b90f10ca33
Damie <jogungbure@gmail.com>
2020-09-25 01:57:59 +0100

    Image testing

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pytesseract
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tessaract OCR\tesseract.exe"
-img = Image.open("img.jpg")
+img = Image.open("hh.JPG")
 text = pytesseract.image_to_string(img)
 print(text)
